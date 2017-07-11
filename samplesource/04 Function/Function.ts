e7adfbdfbcb453f5476509aa4fc8b704b4e870d6
liuborama <liuborama@gmail.com>
2017-07-11 09:05:03 +0800

    ts

---
+++ samplesource/04 Function/Function.ts
@@ -0,0 +1,12 @@
+class MyMath {
+  constructor () {}
+  add(x: number, y: number = 100, z?: number): number {
+    if (z == null) {
+        return x + y;
+    } else {
+        return x + y + z;
+    }
+  }
+}
+let math = new MyMath();
+document.body.innerHTML = math.add(1).toString();
