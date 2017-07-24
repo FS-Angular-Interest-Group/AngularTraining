58215b10bc14e21a3f9b7a927ffa2d1dc1648bfc
Liu <bo.e.liu@accenture.com>
2017-07-24 16:00:02 +0800

    Add 07 PPT and Sample

---
+++ samplesource/07/pipes/src/app/fetch-json.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Http }                from '@angular/http';
+
+import 'rxjs/add/operator/map';
+
+@Pipe({
+  name: 'fetch',
+  pure: false
+})
+export class FetchJsonPipe  implements PipeTransform {
+  private cachedData: any = null;
+  private cachedUrl = '';
+
+  constructor(private http: Http) { }
+
+  transform(url: string): any {
+    if (url !== this.cachedUrl) {
+      this.cachedData = null;
+      this.cachedUrl = url;
+      this.http.get(url)
+        .map( result => result.json() )
+        .subscribe( result => this.cachedData = result );
+    }
+
+    return this.cachedData;
+  }
+}
