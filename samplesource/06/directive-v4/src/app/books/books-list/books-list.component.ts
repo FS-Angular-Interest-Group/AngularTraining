ccc6dcb5badc604ecc89c90c19032a27e4e883f9
Zhang, Zhongpeng <zhongpeng.zhang@accenture.com>
2017-08-03 09:05:29 +0800

    add proxy

--- samplesource/06/directive-v4/src/app/books/books-list/books-list.component.ts
+++ samplesource/06/directive-v4/src/app/books/books-list/books-list.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+
+
 @Component({
   selector: 'app-books-list',
   templateUrl: './books-list.component.html',
@@ -8,22 +13,19 @@ import { Component, OnInit } from '@angular/core';
 export class BooksListComponent implements OnInit {
   books: Array<any> = [];
   selectedBook:any;
-  constructor() { 
-    this.books = [{
-      'name': 'Java',
-      'description': 'Java is a programming language and is commonly used for developing and delivering content on the Web. ',
-      'imagePath': 'http://px.thea.cn/Public/Upload/512687/Intro/1461059814.jpg'
-    }, {
-      'name': 'Swift',
-      'description': 'Swift expert Jack Watson-Hamblin walks through the basics of building an iOS app in Apple\'s new language',
-      'imagePath': 'http://img2.imgtn.bdimg.com/it/u=115915198,2259288664&fm=26&gp=0.jpg'
-    }, {
-      'name': 'Kotlin',
-      'description': 'Kotlin is a statically typed programming language for the JVM, Android and the browser',
-      'imagePath': 'http://img1.imgtn.bdimg.com/it/u=1011491324,3927154791&fm=11&gp=0.jpg'
-    }];
+  constructor(private http: Http) {
   }
+
   ngOnInit() {
+    setTimeout(() => {
+      this.queryData();
+    }, 1500);
+  }
+
+  queryData() {
+    this.http.get('/api/books').map((res: Response) => res.json().data).subscribe(data => {
+      this.books = data;
+    });
   }
 
 }
